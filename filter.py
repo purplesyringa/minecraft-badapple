0b71b049b0bab7cfcf58113328d16de10aa5ba4d
Alisa Sireneva <me@purplesyringa.moe>
2024-09-13 20:37:32 +0300

    Add filter.py

---
+++ filter.py
@@ -0,0 +1,101 @@
+import os
+import json
+from PIL import Image, ImageDraw
+
+
+ROOT = "/baka/minecraft/resourcepacks/Bad Apple!!/assets"
+COLORS = [0, 9, 27, 49, 76, 106, 139, 175, 214, 255]
+
+
+os.makedirs(f"{ROOT}/minecraft/blockstates")
+os.makedirs(f"{ROOT}/badapple/models/block")
+os.makedirs(f"{ROOT}/badapple/textures/block")
+
+
+with open("opaqueblocks") as f:
+    for i, name in enumerate(f):
+        name = name.strip()
+        if i == len(COLORS) ** 2:
+            break
+
+        with open(f"{ROOT}/minecraft/blockstates/{name}.json", "w") as f1:
+            json.dump({
+                "variants": {
+                    "": {
+                        "model": f"badapple:block/{name}"
+                    }
+                }
+            }, f1)
+
+        with open(f"{ROOT}/badapple/models/block/{name}.json", "w") as f1:
+            json.dump({
+                "ambientocclusion": False,
+                "elements": [
+                    {
+                        "from": [0, 0, 16],
+                        "to": [8, 16, 16],
+                        "faces": {
+                            "north": {"texture": "#front"},
+                            "south": {"texture": "#front"}
+                        }
+                    },
+                    {
+                        "from": [0, 0, 0],
+                        "to": [16, 16, 0],
+                        "faces": {
+                            "north": {"texture": "#back"},
+                            "south": {"texture": "#back"}
+                        }
+                    }
+                ],
+                "textures": {
+                    "front": f"badapple:block/{name}",
+                    "back": "minecraft:block/cobblestone"
+                }
+            }, f1)
+
+        im = Image.new("L", (16, 16))
+        draw = ImageDraw.Draw(im)
+        draw.rectangle((0, 0, 15, 7), fill=COLORS[i % len(COLORS)])
+        draw.rectangle((0, 8, 15, 15), fill=COLORS[i // len(COLORS)])
+        im.save(f"{ROOT}/badapple/textures/block/{name}.png", "PNG")
+
+
+with open("transparentblocks") as f:
+    for i, name in enumerate(f):
+        name = name.strip()
+        if i == len(COLORS) ** 2:
+            break
+
+        with open(f"{ROOT}/minecraft/blockstates/{name}.json", "w") as f1:
+            json.dump({
+                "variants": {
+                    "": {
+                        "model": f"badapple:block/{name}"
+                    }
+                }
+            }, f1)
+
+        with open(f"{ROOT}/badapple/models/block/{name}.json", "w") as f1:
+            json.dump({
+                "ambientocclusion": False,
+                "elements": [
+                    {
+                        "from": [8, 0, 0],
+                        "to": [16, 16, 0],
+                        "faces": {
+                            "north": {"texture": "#back"},
+                            "south": {"texture": "#back"}
+                        }
+                    }
+                ],
+                "textures": {
+                    "back": f"badapple:block/{name}"
+                }
+            }, f1)
+
+        im = Image.new("L", (16, 16))
+        draw = ImageDraw.Draw(im)
+        draw.rectangle((0, 0, 15, 7), fill=COLORS[i % len(COLORS)])
+        draw.rectangle((0, 8, 15, 15), fill=COLORS[i // len(COLORS)])
+        im.save(f"{ROOT}/badapple/textures/block/{name}.png", "PNG")
